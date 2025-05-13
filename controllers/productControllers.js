fbe70b62c982b15464b3c7260a3ac235c23707e9
kmdtaufik <kmdtaufik@noreplygihub.com>
2025-05-13 20:40:01 +0600

    feat: added CRUD operations

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,12 +1,33 @@
 const Product = require("../models/product");
 
 const createProduct = async (req, res) => {
-  // console.log(req.body);
   const newProduct = new Product(req.body);
   const savedProduct = await newProduct.save();
   res.status(201).json(savedProduct);
 };
 
+const getProducts = async (req, res) => {
+  const products = await Product.find();
+  res.status(200).json(products);
+};
+
+const updateProduct = async (req, res) => {
+  const productId = req.params.id;
+  const updatedProduct = await Product.findByIdAndUpdate(productId, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedProduct);
+};
+
+const deleteProduct = async (req, res) => {
+  const productId = req.params.id;
+  await Product.findByIdAndDelete(productId);
+  res.status(204).json();
+};
+
 module.exports = {
   createProduct,
+  getProducts,
+  updateProduct,
+  deleteProduct,
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { createProduct } = require("../controllers/productControllers");
-
+const {
+  createProduct,
+  getProducts,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/productControllers");
+//CRUD operations
 router.post("/", createProduct);
+router.get("/", getProducts);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 module.exports = router;
